92a2effa19bb7badb4796833911cfb7f929b5a5a
Gerald <geraldarzola@gmail.com>
2020-12-11 14:04:02 -0500

    commit tree 886c2ead9c9828286e7e7143b025af1a9339c8ad

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    newarr =[]
+    array.each do |name|
+        newarr << "Hello, my name is #{name}."
+    end
+    return newarr
+end
+
+def assign_rooms(array)
+    roomarr=[]
+    array.each.with_index(1) do |name, index|
+        roomarr << "Hello, #{name}! You'll be assigned to room #{index}!"
+
+    end
+    return roomarr
+end
+
+def printer(x)
+    batch_badge_creator(x).each {|greet| puts "#{greet}"}
+    assign_rooms(x).each {|x| puts "#{x}"}
+end
+
